c855f419a5c4a304877a03c944b8f0f707a78f31
Your Name <you@example.com>
2024-04-19 17:35:55 +0100

    React Redux Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import CakeContainer from './components/CakeContainer';
+import { Provider } from 'react-redux';
+import store from './redux/cakes/store';
 
 
 function App() {
 
   return (
+    <Provider store={store}>
     <div>
-    
-    
+    <CakeContainer/>
     </div>
+    </Provider>
     
   );
 }
